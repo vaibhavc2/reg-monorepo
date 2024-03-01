5146411b1164762d69402697e239e3bc97b82499
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-03-02 00:18:38 +0530

    feat(api): add utils
    add utils needed throughout the project

---
+++ apps/api/src/utils/api-error.util.ts
@@ -0,0 +1,56 @@
+// custom class for error-handling in APIs
+
+// ApiError inherits everything from Error class, but with additional properties
+class ApiError extends Error {
+  statusCode: number;
+  data: null;
+  message: string;
+  success: boolean;
+  errors: any;
+
+  constructor(
+    statusCode: number,
+    message = 'Something went wrong',
+    errors = [],
+    stackTrace = '',
+  ) {
+    super(message); // calls parent class constructor
+
+    this.statusCode = statusCode || 500;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stackTrace) {
+      this.stack = stackTrace;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+class RequiredBodyError extends ApiError {
+  constructor(notIncludedFields: string[]) {
+    super(400);
+    this.message = `Missing fields: ${notIncludedFields.join(
+      ', ',
+    )}. Please fill in all the required fields.`;
+  }
+}
+
+class UnauthorizedError extends ApiError {
+  constructor() {
+    super(401, 'Unauthorized request!');
+  }
+}
+
+class UserNotFoundError extends ApiError {
+  constructor() {
+    super(404, 'User not found!');
+  }
+}
+
+export { RequiredBodyError, UnauthorizedError, UserNotFoundError };
+
+export default ApiError;
---
+++ apps/api/src/utils/generate-tokens.util.ts
@@ -0,0 +1,19 @@
+export const generateTokens = async (user: any) => {
+  // generate access token and refresh token
+  const accessToken = await user.generateAccessToken();
+  const refreshToken = await user.generateRefreshToken();
+
+  // save refresh token to db
+  user.refreshToken = refreshToken;
+
+  // save user
+  const result = await user.save({ validateBeforeSave: false });
+
+  // check if refresh token was saved successfully
+  if (!result) {
+    throw new Error('Unable to save refresh token!');
+  }
+
+  // return tokens
+  return { accessToken, refreshToken };
+};
