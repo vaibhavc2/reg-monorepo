8dd70bef7f12ba9dfc50710f9eb50861a29912ae
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-03-13 19:06:52 +0530

    refactor(api): app-health handler retrieves local time

--- apps/api/src/handlers/app-health.handler.ts
+++ apps/api/src/handlers/app-health.handler.ts
@@ -1,13 +1,13 @@
-import { ApiError } from '@/utils';
 import { appHealthMessages } from '@reg/messages';
 
 export const appHealthHandler = async () => {
-  if (appHealthMessages.success) throw new ApiError(501, 'Not implemented');
+  const currentTime = new Date().toLocaleTimeString();
+
   return {
     status: 200 as 200,
     body: {
       status: 200,
-      message: appHealthMessages.success,
+      message: `${appHealthMessages.success}. Current time is ${currentTime}`,
     },
   };
 };
